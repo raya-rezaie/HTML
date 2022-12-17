a0c589f76074ace77201550d57b0460c34f74225
raya-rezaie <rayarezaie83@gmail.com>
2022-12-17 22:23:02 +0330

    homework08

---
+++ home.php
@@ -0,0 +1,31 @@
+﻿ <html>
+  <title>ارسال</title>
+  <body bgcolor ="lightcyan">
+<html dir="rtl"> 
+   <?php
+   $lastname = $_POST["lname"] . PHP_EOL;
+   $name = $_POST["fname"] . PHP_EOL; 
+   $email = $_POST["email"] . PHP_EOL;
+   $msg = $_POST["message"] . PHP_EOL; 
+   
+   echo ($name) ;
+   echo "  ";
+   echo ($lastname);
+   echo "، پیام شما ارسال شد";
+	
+$fn = 1;
+$newfn=0;
+while(!$newfn){
+    if(!file_exists("$fn.txt"))
+        $newfn = "$fn.txt";
+        $fn++;
+}
+
+file_put_contents("$newfn", $name,FILE_APPEND);
+file_put_contents("$newfn", $lastname,FILE_APPEND);
+file_put_contents("$newfn", $email,FILE_APPEND);
+file_put_contents("$newfn", $msg,FILE_APPEND);
+
+  ?>
+  </body>
+ </html>
